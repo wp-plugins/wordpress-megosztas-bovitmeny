d131c22966f0e11baa8eae0d846f30a5ad3b0f42
KardiWeb <KardiWeb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-24 19:29:42 +0000

    Upload new files
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-megosztas-bovitmeny/trunk@731093 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/oldalsav.php
@@ -0,0 +1,68 @@
+<div id="postbox-container-1" class="postbox-container">
+<div class="meta-box-sortables">
+<!-- Oldalsáv támogatás -->
+<div class="postbox">
+<h3><span>Támogatás</span></h3>
+<div class="inside">
+<p>Hiba esetén kérem vegye fel velem a <script>//<![CDATA[
+     eval(unescape('%70%6c%75%6d%69%34%31%3d%5b%27%25%36%62%25%37%37%27%2c%5b%27%25%36%66%25%37%32%25%36%37%27%2c%27%25%36%62%25%36%31%25%37%32%25%36%34%25%36%39%25%37%37%25%36%35%25%36%32%27%5d%2e%72%65%76%65%72%73%65%28%29%2e%6a%6f%69%6e%28%27%2e%27%29%5d%2e%6a%6f%69%6e%28%27%40%27%29%3b%65%77%70%6f%70%30%37%3d%27%6b%61%70%63%73%6f%6c%61%74%27%3b%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%65%77%70%6f%70%30%37%2e%6c%69%6e%6b%28%27%6d%61%69%27%2b%27%6c%74%6f%3a%27%2b%70%6c%75%6d%69%34%31%29%29%3b'))
+     //]]></script>-ot, hogy mielőbb el tudjam végezni a javítást.
+     <br /><br />
+     Amennyiben tetszik a wordpress megosztás bővítmény, kérem fontolja meg a támogatást.
+     <br />
+     <center><form target="_blank" action="https://www.paypal.com/cgi-bin/webscr" method="post"><div class="paypal-donations"><input type="hidden" name="cmd" value="_donations" /><input type="hidden" name="business" value="&#108;&#097;&#115;&#122;&#108;&#111;&#046;&#101;&#115;&#112;&#097;&#100;&#097;&#115;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;" /><input type="hidden" name="return" value="http://www.kardiweb.org/" /><input type="hidden" name="item_name" value="Laszlo Espadas" /><input type="hidden" name="item_number" value="KardiWeb www.kardiweb.org" /><input type="hidden" name="currency_code" value="HUF" /><input type="image" src="<?php bloginfo('url'); ?>/wp-content/plugins/wordpress-megosztas-bovitmeny/img/tagmogatas.png" name="submit" alt="PayPal támogatás" /><img alt="" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" /></div></form></center>
+</p>
+<p><center>Copyright &copy; 1986 - <?php echo date('Y'); ?><br><a target="_blank" href="http://www.kardiweb.org">Laszlo Espadas (KardiWeb)</a></center></p>
+</div>
+</div>
+<!-- /Oldalsáv támogatás -->
+
+<!-- Oldalsáv hasznos hivatkozások -->
+<div class="postbox">
+<h3><span>Hasznos hivatkozások</span></h3>
+<div class="inside">
+<p>
+     <ul>
+     <li><a target="_blank" href="https://www.digitalocean.com/?refcode=af5b1d37263e">Olcsó SSD Tárhely WordPressnek</a></li>
+     <li><a target="_blank" href="http://forditas.kardiweb.org/">KardiWeb Fordító Központ</a></li>
+     <li><a target="_blank" href="http://www.wordpress.org/">WordPress.org</a></li>
+     <li><a target="_blank" href="http://www.wphu.org/">WordPress Magyarország</a></li>
+     <li><a target="_blank" href="http://sugo.wphu.org/">Magyar WordPress Dokumentáció</a></li>
+     <li><a target="_blank" href="http://codex.wordpress.org/1">Angol WordPress Dokumentáció</a></li>
+     <li><a target="_blank" href="http://blog.wphu.org/">Magyar WordPress Blog</a></li>
+     <li><a target="_blank" href="http://kozosseg.wphu.org/forumok">Magyar WordPress Fórum</a></li>
+     <li><a target="_blank" href="http://wordpress.org/support/">Angol WordPress Fórum</a></li>
+     <li><a target="_blank" href="http://hu.wordpress.com/">WordPress.com ingyenes blog</a></li>
+     <li><a target="_blank" href="http://www.kardiweb.org/">Laszlo Espadas (KardiWeb)</a></li>
+     <li><a target="_blank" href="http://themeforest.net/category/wordpress/?ref=kardiweb">Premium sablonok</a></li>
+     <li><a target="_blank" href="http://codecanyon.net/category/wordpress/?ref=kardiweb">Premium bővítmények</a></li>
+     <li><a target="_blank" href="http://www.wordpress.org/extend/plugins/">Ingyenes bővítmények</a></li>
+     <li><a target="_blank" href="http://www.wordpress.org/etend/themes/">Ingyenes sablonok</a></li>
+     <li><a target="_blank" href="http://www.mobiltelo.hu/490282">Ingyen sms küldés</a></li>
+     <li><a target="_blank" href="http://www.faceblogger.hu/">Faceblogger.hu</a></li>
+     <li><a target="_blank" href="http://translate.wordpress.com/projects/wpcom/hu/default">WordPress.com fordítás</a></li>
+     <li><a target="_blank" href="http://translate.wordpress.com/projects/jetpack">Jetpack fordítás</a></li>
+     <li><a target="_blank" href="http://translate.wordpress.com/projects/gravatar/hu/default">Gravatar fordítás</a></li>
+     <li><a target="_blank" href="http://translate.wordpress.com/projects/polldaddy">Polldaddy fordítás</a></li>
+     </ul>
+</p>
+</div>
+</div> <!-- .postbox -->
+<!--/Oldalsáv hasznos hivatkozások-->
+
+<!-- Oldalsáv licenc -->
+<div class="postbox">
+<h3><span>Licenc</span></h3>
+<div class="inside">
+<p style="text-align:justify">A WordPress Megosztás Bővítmény programozása, karbantartása jelentős munkát igényelt és igényel, ezért alkottam meg az alábbi licenct:
+     <ul>
+     <li><em> 1. A felhasználó ingyenesen használhatja, telepítheti a bővítményeket saját weboldalára, blogjára. A fájlokba igénye szerint belejavíthat.</em></li>
+     <li><em> 2. A bővítményt a GPL/GNU licenc értelmében nem értékesítheti, letöltésként nem publikálhatja. Közvetlen publikálás helyett javaslom hivatkozás használatát a bemutatkozó oldalamhoz!</em></li>
+     </ul>
+</p>
+</div>
+</div> <!-- .postbox -->
+<!-- /Oldalsáv licenc -->
+
+</div> <!-- .meta-box-sortables -->
+</div> <!-- #postbox-container-1 .postbox-container -->
