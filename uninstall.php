1372736b149531a6c4996c32788741431dac57dd
KardiWeb <KardiWeb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-24 19:13:08 +0000

    Update files
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-megosztas-bovitmeny/trunk@731073 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+
+/* MEGOSZTÁS BŐVÍTMÉNY BEÁLLÍTÁSOK ELTÁVOLÍTÁSA AZ ADATBÁZISBÓL */
+
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+exit;
+// Beállítások ellenörzése és tőrlése
+if ( get_option( 'kwm_beallitasok' ) != false ) {
+delete_option( 'kwm_beallitasok' );
+}
+?>
